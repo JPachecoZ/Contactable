776d4e2f4ff23bd2278c09ce6fc7c4ff4fc78d51
Alonso <alonsozf.94@gmail.com>
2022-02-25 15:39:14 -0500

    Finished all fetch functions

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-import { deleteContact, updateContact } from "./scripts/services/contacts-service.js";
+import { createContact, getContact, getContacts, deleteContact, updateContact, toggleFavorite } from "./scripts/services/contacts-service.js";
 import { login, signup } from "./scripts/services/session-service.js";
 
 const credentials = {
-  email: "davis@mail.com",
+  email: "steve@mail.com",
   password: "123456"
 }
 
-let data = {
-  name: "holaaa",
-  number: "111111111",
+let contactData = {
+  name: "Dude",
   email: "hola@react.com",
+  number: "123456789",
   relation: "Friends",
-  favorite: true
 }
 
 async function test() {
   try {
     const myUser = await login(credentials);
+    //const newContact = await createContact(contactData);
+    const contact = await toggleFavorite(570)
     console.log(myUser);
-   // deleteContact("545")
-   updateContact("547",data)
+    console.log(contact);
   } catch (error) {
     console.log(error.message);
   }
 }
-
-
 
 test();
