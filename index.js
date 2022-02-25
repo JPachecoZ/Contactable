c69d9cb175beca59a28d6bd90b62270dfcf96702
davis devops <davis.espinoza.garay@gmail.com>
2022-02-25 01:54:48 -0500

    add service deleteContact, updateContact

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
+import { deleteContact, updateContact } from "./scripts/services/contacts-service.js";
 import { login, signup } from "./scripts/services/session-service.js";
 
 const credentials = {
-  email: "steve@mail.com",
+  email: "davis@mail.com",
   password: "123456"
+}
+
+let data = {
+  name: "holaaa",
+  number: "111111111",
+  email: "hola@react.com",
+  relation: "Friends",
+  favorite: true
 }
 
 async function test() {
   try {
-    const myUser = await signup(credentials);
+    const myUser = await login(credentials);
     console.log(myUser);
+   // deleteContact("545")
+   updateContact("547",data)
   } catch (error) {
     console.log(error.message);
   }
 }
+
+
 
 test();
