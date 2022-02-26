79ead7ac724368dccbe85291bd5c35086695b396
Javier <jepach@gmail.com>
2022-02-26 13:45:07 -0500

    Add ContactDetails page at contact-page

--- scripts/pages/contact-page.js
+++ scripts/pages/contact-page.js
@@ -3,24 +3,41 @@ import DOMHandler from "../dom-handler.js";
 import STORE from "../store.js";
 
 // Services
+import { logout } from "../services/session-service.js";
+
 // Pages
+import HomePage from "./home-page.js";
+import LoginPage from "./login-page.js";
 
 // Draws page
 function render() {
+  const contact = STORE.contacts.find((element) => element.id == STORE.currentContactId);
+  console.log(contact);
   return `
     <section style="position: relative">
       <div class="header">
         <h1>Contact Detail</h1>
         <a class="logout-link">Logout</a>
       </div>
-      <div class="container">
-        <h2>Contenido de contacto con id ${STORE.currentContactId}</h2>
-        <br>
-        <ul>
-          <li>Aqui es solo informativo y las opciones del footer</li>
-          <li>Copiarse el footer del login o signup el container se llama footer-links</li>
-          <li>Al momento de regresar al home asignar el STORE.currentContactId a nulo </li>
-        </ul>
+      <div class="container contact-detail">
+        <img src="/img/Profile.svg" class="show-contact"/>
+        <span class="contact-name">${contact.name}</span>
+        <span class="contact-data">${contact.relation}</span>
+        <div class="contact-container">
+        <div class="contact-data">
+          <span class="contact-attr">Number: </span>
+          <span class="contact-value">${contact.number}</span>
+        </div>
+        <div class="contact-data">
+          <span class="contact-attr">Email: </span>
+          <span class="contact-value">${contact.email}</span>
+        </div>
+        </div>
+        <div class="footer-links">
+          <a class="to-home">Back</a>
+          <a class="to-delete">Delete</a>
+          <a class="to-edit">Edit</a>
+        </div>
       </div>
     </section>
   `
@@ -28,12 +45,34 @@ function render() {
 
 // Creates functions for page listeners
 
+function listenLogout() {
+  const logoutButton = document.querySelector(".logout-link")
+
+  logoutButton.addEventListener("click", async (event) => {
+    event.preventDefault();
+    try {
+      await logout();
+      DOMHandler.load(LoginPage)
+    } catch (error) {
+      console.log(error)
+    }
+  });
+}
+
 function listenToEditContact() {
   console.log("Se va al page editar contacto")
 }
 
 function listenGoBack() {
-  console.log("Se vuelve al home")
+  try {
+    const toHomeLink = document.querySelector(".to-home");
+
+    toHomeLink.addEventListener("click", () => {
+      DOMHandler.load(HomePage);
+    })
+  } catch (error) {
+    console.log(error.message)
+  }
 }
 
 function listenDelete() {
@@ -47,6 +86,7 @@ const ContactPage = {
     return render();
   },
   addListeners() {
+    listenLogout();
     listenToEditContact();
     listenGoBack();
     listenDelete();
