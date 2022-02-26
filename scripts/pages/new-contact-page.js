55d1542f0bfe24c08d21c2b3eb265db74a59dc53
Javier <jepach@gmail.com>
2022-02-26 11:26:01 -0500

    Add content to NewContactPage

--- scripts/pages/new-contact-page.js
+++ scripts/pages/new-contact-page.js
@@ -1,12 +1,15 @@
 // Helpers
 import DOMHandler from "../dom-handler.js";
 import STORE from "../store.js";
+import { createContact } from "../services/contacts-service.js";
 
 // Services
 // Pages
 
 // Draws page
 function render() {
+  // const { createError } = NewContactPage.state
+  // ${createError ? `<p> ${createError}</p>` : ""}
   return `
     <section style="position: relative">
       <div class="header">
@@ -14,15 +17,23 @@ function render() {
         <a class="logout-link">Logout</a>
       </div>
       <div class="container">
-        <h2>Contenido</h2>
-        <br>
-        <ul>
-          <li>Es parecido al login agregenle un form acá</li>
-          <li>Dentro del mismo form agregar el footer que esta en el render de login con los botones cancel y save</li>
-          <li>Los listeners para cada boton ya están creados</li>
-          <li>El cancel debe llevarte al home de nuevo y el save guardar duh</li>
-          <li>El edit es casi igual a esto</li>
-        </ul>
+        <form class="form newcontact-form">
+          <input type="text" id="Name" name="Name" required placeholder="Name" />
+          <input type="text" id="Number" name="Number" required placeholder="Number" />
+          <input type="text" id="Email" name="Email" required placeholder="Email" />
+          <select id="Relation" name="Relation" required >
+            <option value="" selected disabled hidden>Relation</option>
+            <option value = "Family">Family</option>
+            <option value = "Friends">Friends</option>
+            <option value = "Work">Work</option>
+            <option value = "Acquaintance">Acquaintance</option>
+          </select>
+          
+          <div class="footer-links">
+          <a class="to-home">Cancel</a>
+          <button class="button button--primary">Save</button>
+          </div>
+        </form>
       </div>
     </section>
   `
@@ -31,7 +42,29 @@ function render() {
 // Creates functions for page listeners
 
 function listenAddContact() {
-  console.log("Se agrega el contato desde el form")
+  const newContactForm = document.querySelector(".newcontact-form");
+
+  newContactForm.addEventListener("submit", async (event) => {
+    try {
+      event.preventDefault();
+      const { name, email, number, relation } = event.target;
+      console.log(event.target);
+      const data = {
+        name: name.value,
+        email: email.value,
+        number: number.value,
+        relation: relation.value,
+      };
+      console.log(data);
+      const result = await createContact(data);
+      console.log(result);
+      alert("Se agrego contacto " + data.name);
+
+    } catch (error) {
+      // NewContactPage.state.signupError = error.message;
+      DOMHandler.reload();
+    }
+  })
 }
 
 function listenCancel() {
@@ -46,7 +79,7 @@ const NewContactPage = {
   },
   addListeners() {
     listenAddContact();
-    listenCancel();
+    //listenCancel();
   }
 }
 
